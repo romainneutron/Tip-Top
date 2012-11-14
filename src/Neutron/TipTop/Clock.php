66a172710a5ba6f7d47fe097565464e424c54e24
Romain Neutron <imprec@gmail.com>
2012-11-14 03:02:12 +0100

    Initial commit

---
+++ src/Neutron/TipTop/Clock.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Neutron\TipTop;
+
+class Clock
+{
+    private $stack = array();
+
+    public function __construct()
+    {
+        $this->initialize();
+        $this->set(1, function(){echo memory_get_usage()."\n";});
+    }
+
+    private function initialize()
+    {
+        declare(ticks = 1);
+        pcntl_signal(SIGALRM, array($this, 'beat'), true);
+        pcntl_alarm((int) 1);
+    }
+
+    public function beat()
+    {
+        $toReset = $toRemove = array();
+
+        foreach ($this->stack as $moment => $timers) {
+            if ($moment > time()) {
+                break;
+            }
+
+            foreach ($timers as $timer) {
+                call_user_func($timer->callback);
+                $timer->iterations--;
+
+                if ($timer->iterations > 0) {
+                    if(!isset($toReset[$moment + $timer->period])) {
+                        $toReset[$moment + $timer->period] = array();
+                    }
+                    array_push($toReset[$moment + $timer->period],$timer);
+                }
+                $toRemove[] = $moment;
+            }
+        }
+
+        foreach ($toRemove as $moment) {
+            unset($this->stack[$moment]);
+        }
+
+        $sort = false;
+
+        foreach ($toReset as $moment => $timers) {
+            if (!isset($this->stack[$moment])) {
+                $this->stack[$moment] = array();
+                $sort = true;
+            }
+            $this->stack[$moment] = array_merge($this->stack[$moment], $timers);
+        }
+
+        if ($sort === true) {
+            ksort($this->stack);
+        }
+
+        pcntl_alarm(1);
+    }
+
+    public function set($seconds, $callback, $iterations = INF)
+    {
+        $moment = time() + $seconds;
+
+        if (!isset($this->stack[$moment])) {
+            $this->stack[$moment] = array();
+            ksort($this->stack);
+        }
+
+        $data = (object) array(
+                'period'     => $seconds,
+                'callback'   => $callback,
+                'iterations' => $iterations,
+        );
+
+        array_push($this->stack[$moment], $data);
+    }
+
+    public function clear()
+    {
+        $this->stack = array();
+    }
+}
